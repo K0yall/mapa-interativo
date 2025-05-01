8e5b2dcf2d7cb4bc5400fa290c70c006824ec9c2
Luan Gerber Siiss <1033720@UNIDAVI.LOCAL>
2025-04-30 21:35:30 -0300

    alterado para utilizar o laravel

---
+++ novo-rumo/resources/views/home/menu.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('title', 'Descubra o Vale')
+@section('content')
+    <div class="content">
+        <h1>Bem-vindo ao Alto Vale do Itajaí</h1>
+        <p>Explore as cidades, compare dados e descubra onde você gostaria de viver.</p>
+    </div>
+    
+@endsection
---
+++ novo-rumo/resources/views/layout/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_',"-", app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/cidades.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/caracteristicas.css') }}">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header class="mac-navbar">
+        <div class="mac-container">
+            <!-- Ícone no canto esquerdo -->
+            <div class="mac-icon">
+                <img src="{{ asset('../img/logo1.png') }}" alt="Ícone" />
+            </div>
+            <!-- Menu centralizado -->
+            <nav class="mac-menu">
+                <ul>
+                    <li><a href="{{ url('Home') }}">Início</a></li>
+                    <li><a href="{{ url('Cidades') }}">Cidades</a></li>
+                    <li><a href="#Comparar">Comparar</a></li>
+                    <li><a href="../../view/map/map.html">Mapa</a></li>
+                    <li><a href="../../view/historia/altovale.html">O Alto Vale do Itajaí</a></li>
+                    <li><a href="#contato">Contato</a></li>
+                    <li><a href="#mapa">Login</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main id="main-conteudo">
+        @yield('content')
+    </main>
+    <script src="js/script.js"></script>
+    <script src="js/Caractetisticas.js"></script>
+</body>
+</html>
